018c17b8d299c22471a63f17acea8db18a6b4254
Akshay Bodhare <abodhare1234@gmail.com>
2019-01-15 18:53:58 +0530

    Completed layout and basic look of app.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
+    const background = {
+      'background-color': '#123456',
+      'min-height': '100vh'
+    };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="container-fluid" >
+          <div className="row" style={background}>
+            <div className="col-1" />
+            <div className="col-auto align-self-center">
+              <div className="card w-100">
+                <div className="card-body">
+                  <blockquote className="blockquote mb-0">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                    <footer className="blockquote-footer">Someone famous</footer>
+                  </blockquote>
+                  <div className="d-flex">
+                    <div className="p-2"><a href="#" className="btn btn-primary"><i className="fab fa-twitter"></i></a></div>
+                    <div className="p-2"><a href="#" className="btn btn-primary"><i className="fab fa-facebook"></i></a></div>
+                    <div className="ml-auto p-2"><a href="#" className="btn btn-primary">New quote</a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-1" />
+          </div>
+        </div>
     );
   }
 }
