d91e00b557f0efb62a938f6fa54fcbe3895899be
Akshay Bodhare <abodhare1234@gmail.com>
2019-01-29 22:13:54 +0530

    Updates

---
+++ precache-manifest.429ac0a18673ebc4a8f248b611ed38a6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "e00a7e07255e71bd14d5",
+    "url": "/random-quote-machine/static/css/main.b5497b38.chunk.css"
+  },
+  {
+    "revision": "e00a7e07255e71bd14d5",
+    "url": "/random-quote-machine/static/js/main.e00a7e07.chunk.js"
+  },
+  {
+    "revision": "81ea4cdfc3ae54874a09",
+    "url": "/random-quote-machine/static/css/1.91cf9949.chunk.css"
+  },
+  {
+    "revision": "81ea4cdfc3ae54874a09",
+    "url": "/random-quote-machine/static/js/1.81ea4cdf.chunk.js"
+  },
+  {
+    "revision": "b8507572375d5b5ea7f9",
+    "url": "/random-quote-machine/static/js/runtime~main.b8507572.js"
+  },
+  {
+    "revision": "e65d96d04378217b8b5caff2fb1b62cb",
+    "url": "/random-quote-machine/static/media/fa-regular-400.e65d96d0.woff2"
+  },
+  {
+    "revision": "fc5252e568503bfb163233ec63b8a2ce",
+    "url": "/random-quote-machine/static/media/fa-regular-400.fc5252e5.woff"
+  },
+  {
+    "revision": "8e9f0ee6d016526776c27f91fea55cca",
+    "url": "/random-quote-machine/static/media/fa-regular-400.8e9f0ee6.eot"
+  },
+  {
+    "revision": "deb8619e67992d3fa088b5dcbb8cff36",
+    "url": "/random-quote-machine/static/media/fa-regular-400.deb8619e.ttf"
+  },
+  {
+    "revision": "b89c6c51aa2999159a6667f97a46a346",
+    "url": "/random-quote-machine/static/media/fa-brands-400.b89c6c51.woff2"
+  },
+  {
+    "revision": "dd4ff1196905f0585aef2c80486869e9",
+    "url": "/random-quote-machine/static/media/fa-brands-400.dd4ff119.woff"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "/random-quote-machine/static/media/fa-solid-900.8086e197.woff2"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "/random-quote-machine/static/media/fa-solid-900.52b8345b.woff"
+  },
+  {
+    "revision": "50a544b5ad4d512e8db3cdc089823a00",
+    "url": "/random-quote-machine/static/media/fa-brands-400.50a544b5.ttf"
+  },
+  {
+    "revision": "7d43b9ae0b8bb1d0dc425d78dc577b0a",
+    "url": "/random-quote-machine/static/media/fa-brands-400.7d43b9ae.eot"
+  },
+  {
+    "revision": "734248d027cbaac7f03c0d931ec95593",
+    "url": "/random-quote-machine/static/media/fa-regular-400.734248d0.svg"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "/random-quote-machine/static/media/fa-solid-900.f72d546a.eot"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "/random-quote-machine/static/media/fa-solid-900.1709a281.ttf"
+  },
+  {
+    "revision": "fef43c1fd872ef7f1880483d9b63ccf2",
+    "url": "/random-quote-machine/static/media/fa-brands-400.fef43c1f.svg"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "/random-quote-machine/static/media/fa-solid-900.27603739.svg"
+  },
+  {
+    "revision": "69cded65211016054623e30af8793bde",
+    "url": "/random-quote-machine/index.html"
+  }
+];
